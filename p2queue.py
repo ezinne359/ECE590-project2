f1fe7f9a387bd86becb6dfff218c3ae6cfb6e134
George Lindner <dglindner2@gmail.com>
2020-10-22 18:39:37 -0400

    finish project

--- p2queue.py
+++ p2queue.py
@@ -2,9 +2,7 @@
 Math 560
 Project 2
 Fall 2020
-
 p2queue.py
-
 Partner 1:
 Partner 2:
 Date:
@@ -49,34 +47,82 @@ class Queue:
     isFull function to check if the queue is full.
     """
     def isFull(self):
-        ##### IMPLEMENT! #####
-        return
+        # returns True is numElems is equal to the length of the queue
+        return self.numElems == len(self.queue)
 
     """
     isEmpty function to check if the queue is empty.
     """
     def isEmpty(self):
-        ##### IMPLEMENT! #####
-        return
+        # returns True is queue is empty so numElems is equal to zero
+        return self.numElems == 0
 
     """
     resize function to resize the queue by doubling its size.
     Note: we also reset the front to index 0.
     """
     def resize(self):
-        ##### IMPLEMENT! #####
+        # Unwrapping
+        # resize queue to be double the size
+        new_length = 2 * len(self.queue)
+        # initialize new array of double the size
+        B = [None for x in range(0, new_length)]
+        k = 0
+        # while the queue is not empty
+        while not self.isEmpty():
+            # pop  values to new list of double the size
+            B[k] = self.pop()
+            k += 1
+
+        # reset the queue and the front values
+        self.queue = B
+        self.front = 0
+
+        # reset the rear values and numElems
+        self.rear = k
+        self.numElems = k
+
         return
 
     """
     push function to push a value into the rear of the queue.
     """
     def push(self, val):
-        ##### IMPLEMENT! #####
+        # As long as the rear is not at the end of queue
+        # then push value to the rear of queue
+        # and update rear and numElems
+        # if the queue is full then first resize the list
+        if self.isFull() is True:
+            self.resize()
+        # push value to the rear of the queue
+        self.queue[self.rear] = val
+        # increment rear value by one
+        self.rear += 1
+        # update rear value to be current rear mod length of the queue
+        # if queue is  not full but rear is at the end of the  list
+        # then this will induce wrap around behavior
+        self.rear = self.rear % len(self.queue)
+        # increment numElems
+        self.numElems += 1
+
         return
 
     """
     pop function to pop the value from the front of the queue.
     """
     def pop(self):
-        ##### IMPLEMENT! #####
-        return None
+        # remove value from front of the queue
+        # update front and numElems
+        # store  value to be popped
+        pop_val = self.queue[self.front]
+        # replace popped value with None
+        self.queue[self.front] = None
+        # increment front
+        self.front += 1
+        # if queue is  not full but front is at the end of the list
+        # then this will induce wrap around behavior
+        self.front = self.front % len(self.queue)
+        # decrease numElems
+        self.numElems -= 1
+
+        return pop_val
--- project2.py
+++ project2.py
@@ -29,45 +29,83 @@ def bdfs(maze, alg):
     if (alg != 'BFS') and (alg != 'DFS'):
         raise Exception('Incorrect alg! Need BFS or DFS!')
 
-    def bdfs(maze, alg):
-        # If the alg is not BFS or DFS, raise exception.
-        if (alg != 'BFS') and (alg != 'DFS'):
-            raise Exception('Incorrect alg! Need BFS or DFS!')
+    maze.path = []
 
-        # Depth First Search
-        if alg == 'DFS':
+    # Step 1: Initialize and Set Flags to False
+    for vertex in maze.adjList:
+        vertex.dist = math.inf  # Infinite dist initially.
+        vertex.visited = False  # Nothing visited at initialization.
+        vertex.prev = None
 
-            # Step 1: Initialize and Set Flags to False
-            m = maze
-            for vertex in m.adjList:
-                vertex.visited = False
-                vertex.prev = None
+    # Depth First Search
+    if alg == 'DFS':
+        # Step 2: Set Start Visited Flag to True, Push start to stack
+        maze.start.visited = True
+        stack = Stack()
+        stack.push(maze.start)
 
-            # Step 2: Set Start Visited Flag to True, Push start to stack
-            m.start.visited = True
-            stack = Stack()
-            stack.push(m.start)
+        # Step 3: While the stack is not empty, loop
+        while stack.numElems > 0:
+            current = stack.pop() # Pop the top element off the stack and observe its neighbors
+            if current == maze.exit:
+                break
 
-            # Step 3:
-            while stack.numElems > 0:
-                current = stack.pop()
-                for neighbor in current.neigh:
-                    if neighbor.visited == False:
-                        neighbor.visited = True
-                        stack.push(neighbor)
-                        neighbor.prev = current
-                        if neighbor == m.exit:
-                            break
+            for neighbor in current.neigh:
+                if neighbor.visited == False:
+                    # Set the visited flag to True for the neighbor
+                    neighbor.visited = True
+                    stack.push(neighbor)
+                    neighbor.prev = current
 
-            last = m.exit
-            m.path.append(m.exit.rank)
-            while last != m.start:
-                m.path.append(last.prev.rank)
-                last = last.prev
-            m.path = m.path[::-1]
 
-    return []
-    ##### Your implementation goes here. #####
+        last = maze.exit
+        maze.path.append(maze.exit.rank)
+        while last != maze.start:
+            maze.path.append(last.prev.rank)
+            last = last.prev
+        maze.path = maze.path[::-1]
+
+    elif alg == 'BFS':
+
+        queue = Queue()
+
+        # Step 2: Mark start vertices as visited and push to queue
+        maze.start.visited = True
+        queue.push(maze.start)
+        maze.start.dist = 0
+
+        # Step 3: While queue is not empty
+        while not queue.isEmpty():
+            # pop the current element
+            current = queue.pop()
+            # for each current neighbor
+            for neighbor in current.neigh:
+                # check the the neighbor has not been visited
+                if neighbor.dist == math.inf:
+                    # mark as visited by incrementing distance attribute
+                    neighbor.dist = current.dist + 1
+                    # push to the queue
+                    queue.push(neighbor)
+                    # reset previous value
+                    neighbor.prev = current
+                    # once the neighbor we are checking is the exit then break
+                    if neighbor == maze.exit:
+                        break
+
+        # Step 4: Return shortest path
+        start = maze.start
+        last = maze.exit
+        # append rank of exit node to path
+        maze.path.append(last.rank)
+        while last != start:
+            # go back through previous values and append to path
+            maze.path.append(last.prev.rank)
+            last = last.prev
+        # return reversed path so that start is at the front of list
+        maze.path = list(reversed(maze.path))
+
+    return maze.path
+
 
 """
 Main function.
